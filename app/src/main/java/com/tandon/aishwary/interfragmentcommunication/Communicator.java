823da98118d0d08eaee6521a1a7c95c52dd49bbf
Aishwary Tandon <ashwarytandon@gmail.com>
2015-10-05 22:46:25 -0400

    This small example shows how to fragments interact with each other via an Activity

---
+++ app/src/main/java/com/tandon/aishwary/interfragmentcommunication/Communicator.java
@@ -0,0 +1,11 @@
+package com.tandon.aishwary.interfragmentcommunication;
+
+/**
+ * Created by Aishwary on 10/5/2015.
+ */
+public interface Communicator {
+
+    public void respond(String data);
+
+
+}
